ed41c694e7a44baf1e007f59f8b8e1d9ea7a53da
saltwalks <ewqdsacxz2345@gmail.com>
2021-05-17 05:12:22 +0900

    root reorganized : add App.js, index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function Root() {
+    return (
+        
+    )
+}
+
+{/* <header>
+<div class="link" id="logo"><a href="index.html"><h1>Thoth</h1><h2>temple of<br>open knowledges</h2></a></div>
+</header>
+<nav>
+<ul>
+  <li class="link"><a href="./webp/index.html">HTML, CSS</a></li>
+  <li class="link"><a href="./python/index.html">Python</a></li>
+  <li class="link"><a href="./ocaml/index.html">OCaml</a></li>
+  <li class="link"><a href="./js/index.html">JavaScript</a></li>
+  <li class="link"><a href="./algorithm/index.html">Algorithm</a></li>
+  <li class="link"><a href="./github/index.html">Git</a></li>
+  <li class="link"><a href="./database/index.html">Database</a></li>
+</ul>
+</nav>
+<footer>
+<div>copyright©saltwalks2021</div>
+</footer> */}
