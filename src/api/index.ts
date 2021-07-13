56dc6cbb6ff1743c85ba005889f5a17235e383d6
saltwalks <ewqdsacxz2345@gmail.com>
2021-07-14 00:29:06 +0900

    Done : POST request / TODO : main page css + routing to sign-in, sign-up

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,16 @@
+import { st } from 'state-types';
+
 const config = {
     baseUrl: 'http://localhost:3000'
 }
 
 export async function getAllNotes() {
     return fetch(`${config.baseUrl}/all`);
+}
+
+export async function postNote(note: st.note) {
+    return fetch(`${config.baseUrl}/new`, {
+        method: 'POST',
+        body: JSON.stringify(note),
+    });
 }
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -3,7 +3,8 @@ import * as api from "../api";
 
 const actions: st.actions = {
     getAllNotes(context) {
-        api.getAllNotes().then(response => {
+        api.getAllNotes()
+        .then(response => {
             if(response.body !== null) {
                 response.json().then(data => {
                     context.commit('addNotes', data);
@@ -11,11 +12,24 @@ const actions: st.actions = {
             }
         }).catch(err => {
             console.error(err);
-        })
+        });
     },
     postNote(context, data) {
-        console.log(data);
-        
+        api.postNote(data as st.note)
+        .then(postRes => {
+            if(postRes.body !== null) {
+                api.getAllNotes()
+                .then(getRes => {
+                    if(getRes.body !== null) {
+                        getRes.json().then(data => {
+                            context.commit('addNotes', data);
+                        });
+                    }
+                });
+            }
+        }).catch(err => {
+            console.error(err);
+        });
     }
 }
 
