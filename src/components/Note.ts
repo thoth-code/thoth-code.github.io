ca5ac37d2dfc592a663eb7d223c08da9e22c989c
saltwalks <ewqdsacxz2345@gmail.com>
2021-07-18 14:06:37 +0900

    DONE : auto indent, auto paired brackets

--- src/components/Note.ts
+++ src/components/Note.ts
@@ -1,5 +1,5 @@
 import Component from "../lib/component";
-import * as strconv from '../modules/parseCode';
+import * as codeUtils from '../modules/codeUtils';
 
 export default class Note extends Component {
     constructor() {
@@ -17,7 +17,7 @@ export default class Note extends Component {
         title.innerText = this.getAttribute('title') as string;
 
         const code = this.children.item(1)?.children.item(0) as HTMLElement;
-        code.innerHTML = strconv.noHTML(props);
+        code.innerHTML = codeUtils.noHTML(props);
 
         const tag = this.children.item(2) as HTMLElement;
         tag.innerText = this.getAttribute('tag') as string;
