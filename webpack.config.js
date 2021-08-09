e06026c5ba8addb506693bc08a3a88e281c4c2db
haeramkeem <ewqdsacxz2345@gmail.com>
2021-08-09 12:48:51 +0000

    DONE : add webpack plugin clean-webpack-plugin, html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { resolve } = require('path');
+
 const config = {
     entry: './src/index.ts',
     output: {
         filename: 'bundle.js',
-        path: __dirname + '/public',
-        publicPath: '/',
+        path: resolve(__dirname, './dist'),
     },
     module: {
         rules: [
-            /* ------------------------------------- Typescript ---------------------------------- */
             {
                 test: /.tsx?$/,
                 loader: 'ts-loader',
             },
-            /* ------------------------------------- Style ---------------------------------- */
             {
                 test: /.css$/,
                 use: ['style-loader', 'css-loader'],
             },
-            /* ------------------------------------- Font ---------------------------------- */
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 loader: 'file-loader',
             },
-            /* ------------------------------------- Image ---------------------------------- */
             {
                 test: /\.(png|jpe?g)$/,
                 loader: 'file-loader',
@@ -30,15 +29,24 @@ const config = {
         ]
     },
     devServer: {
-        contentBase: __dirname + '/public',
+        contentBase: resolve(__dirname, '/public'),
         historyApiFallback: true,
         hot: true,
         open: true,
         overlay: true,
+	    host: '0.0.0.0',
+	    disableHostCheck: true,
     },
     resolve: {
         extensions: ['.js', '.json', '.ts', '.tsx', '.css']
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            favicon: './static/favicon.ico',
+            template: './static/index.html'
+        }),
+        new CleanWebpackPlugin(),
+    ]
 }
 
 module.exports = config;
