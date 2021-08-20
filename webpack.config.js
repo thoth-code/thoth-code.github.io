71e6fa9efbdc72dbce0d7dad7d322a6a5eb6c089
haeramkeem <ewqdsacxz2345@gmail.com>
2021-08-20 07:29:40 +0000

    modify build destination

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const config = {
     entry: resolve(__dirname, 'src/index.ts'),
     output: {
         filename: 'bundle.js',
-        path: resolve(__dirname, 'dist'),
+        path: resolve(__dirname, 'public'),
         publicPath: '/',
     },
     module: {
